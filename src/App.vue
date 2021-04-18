a91be4f8be0c796374a2d52a4df4eada80e782e9
pontakornth <pontakorn61@gmail.com>
2021-04-18 22:58:54 +0700

    feat: Implement fetching

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+ <h1 class="font-bold text-5xl">DEV posts</h1>
+ <p v-if="error" class="text-red-500">{{error}}</p>
+ <p v-if="!articlesData" class="text-indigo-400">Loading...</p>
+ <main v-else class="container py-4 gap-4 mx-auto grid grid-cols-1 lg:grid-cols-3">
+   <article-card v-for="article in articlesData"
+     :key="article.id"
+     :link="article.url"
+     :imgSrc="article.social_image"
+     :title="article.title"
+     />
+ </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import useSWRV from 'swrv'
+import ArticleCard from './components/ArticleCard.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ArticleCard
+  },
+  setup() {
+    const { data, error } = useSWRV("https://dev.to/api/articles")
+    return {
+       articlesData: data,
+       error
+    }
+  },
 })
 </script>
 
