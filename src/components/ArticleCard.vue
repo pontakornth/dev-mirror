6a23ce408832e4256889071e4143aac6ac5deb98
pontakornth <pontakorn61@gmail.com>
2021-04-18 22:58:45 +0700

    feat: Add article card

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="border border-green-400 shadow-md p-4">
+        <img class="block w-full" :src="imgSrc" />
+        <h1 class="font-bold py-4 text-4xl"><a :href="link">{{ title }}</a></h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        imgSrc: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
